6b62d0793bc026133ac71a50b8f926d662b78481
Rakib Hasan <rakibhasan1808@gmail.com>
2025-10-23 23:29:46 +0600

    update loop

---
+++ loop/while.js
@@ -0,0 +1,16 @@
+let num = 1;
+
+while (num < 5) {
+    console.log ("Loop cholbe", num);
+    num++;
+}
+
+
+
+
+let a = "0";
+
+while (a < 5) {
+    console.log ("amar sonar Bangla"); 
+    a++  ; 
+}
