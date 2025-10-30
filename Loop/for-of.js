1c6785d67597f1c829510dd89de45080dce2b8b8
Rakib Hasan <rakibhasan1808@gmail.com>
2025-10-31 01:28:47 +0600

    add for-of.js

---
+++ Loop/for-of.js
@@ -0,0 +1,15 @@
+const numbers = [5, 8, 10, 15, 20, 25, 30]
+console.log (numbers)
+for (const num of numbers) {
+    console.log (num)
+}
+
+
+
+
+
+const man = ['Rakib', 'Sakib', 'Araf', 'Mehedi', 'Subrata', 'Ripon']
+console.log(man)
+for (const name of man) {
+    console.log (name)
+}
