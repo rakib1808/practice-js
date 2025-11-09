d7c60f06c271279c49af3723c4807d01542aa1b3
Rakib Hasan <rakibhasan1808@gmail.com>
2025-11-10 00:46:46 +0600

    added slice.js file

---
+++ String Object/slice.js
@@ -0,0 +1,26 @@
+        // slice()   //
+//**  Syntax (format)
+//array.slice(start, end)
+
+
+const numbers = [10, 20, 30, 40, 50];
+const part = numbers.slice(1, 4);
+console.log(part);
+//array.slice(start, end) start কোন index থেকে শুরু হবে, end কোন index পর্যন্ত যাবে (exclude করবে — মানে end index-এর আগ পর্যন্ত) ।
+
+
+
+//............................//
+const fruits = ["apple", "banana", "mango", "orange", "grape"];
+console.log(fruits.slice(-3));
+//(-3) মানে শেষ দিক থেকে ৩টা উপাদান।
+
+
+//--------যদি end না দেওয়া হয়----
+const letters = ['a', 'b', 'c', 'd', 'e'];
+console.log(letters.slice(2));
+// index 2 থেকে শুরু করে শেষ পর্যন্ত নিয়ে নেয়।
+
+
+
+
